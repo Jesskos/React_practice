780d70035b072a6eb42e11de52796fdd89e0dc01
Jessica Koscheka <jesskos@gmail.com>
2018-12-20 16:34:40 -0500

    added a Person component and imported it in App

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from "./Person/Person"
 
 class App extends Component {
   render() {
+    // 'class' cant be used since its a reserved word in JS
     return (
       <div className="App">
         <h1>My React App</h1>
+        <p> This is working </p>
+        <Person />
       </div>
       );
-    return React.createElement('div', {className: "App"}, React.createElement('h1', null, 'Does this work now?'))
+    // return React.createElement('div', {className: "App"}, React.createElement('h1', null, 'Does this work now?'))
   }
 }
 
---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,17 @@
+// we dont need to import component 
+// since we are not creating a class that extends component, instead, we are creating a function 
+ // './' indicates relative path
+import React from "react";
+
+
+
+// a component is a function which returns some jsx
+// advantages of using arrow function is using this keyword
+// a compoenet is a function returning some jsx
+// we don
+
+const person = () => {
+	return <p>I am a Person</p>
+}
+
+export default person
